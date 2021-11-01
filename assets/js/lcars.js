785027f70ab6a07e244d1272387b1319c0856361
Arthur Zey <arthur@deltawerx.com>
2021-10-31 19:02:19 -0600

    Added thelcars.com template files with my edits

---
+++ assets/js/lcars.js
@@ -0,0 +1,70 @@
+document.addEventListener("touchstart", function() {},false);
+
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('.scroll-top a').fadeIn();
+    } else {
+        $('.scroll-top a').fadeOut();
+    }
+});
+
+// Arthur Zey 2021-10-31: The following is if only if we're scrolling within the content div, and we want bar-10 to fade in and out for scolling to the top.
+$('.content').scroll(function() {
+  var contentHeight = $('.content').scrollTop();
+  if (contentHeight > 100) {
+    // $('.scroll-top-bar a').css("display", "inline-block");
+    $('.scroll-top-bar a').fadeIn();
+    $('.scroll-top-bar a').css("display", "inline-block");
+  } else {
+    $('.scroll-top-bar a').fadeOut();
+  }
+});
+
+$(document).ready(function() {
+    $("#scroll-top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+});
+
+// Arthur Zey 2021-10-31: The following makes the a tag clickable to bring the content div's scroll position to the top.
+$(document).ready(function() {
+    $("#scroll-top-bar").click(function(event) {
+        event.preventDefault();
+        $('.content').animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+});
+
+// Arthur Zey 2021-10-31: The following corrects the overall window position if we want the content div to scroll, but we got to the page with an anchor link in the URL:
+// window.location.hash.substr(1).length > 0 tells us that the loaded URL had an anchor link
+// $(window).scrollTop() > 0 tells us that the overall page is scrolled (the default for the template)
+// $("body").css("overflow") == "hidden" tells us that the overall page's overflow is hidden; ie, that the content div is going to scroll (this is set in the CSS)
+$(document).ready(function() {
+  if (window.location.hash.substr(1).length > 0 && $(window).scrollTop() > 0 && $("body").css("overflow") == "hidden") {
+    window.scrollTo(0,0);
+  }
+});
+
+// Arthur Zey 2021-10-31: The following makes internal page anchor links work correctly and scroll smoothly.
+// First, we trigger on a click on any link.
+// Second, we check if the target URL (without the hash) is the same as the current page's URL (without the hash), which tells us whether it's an internal anchor link.
+$(document).ready(function() {
+  $('a').click(function(event) {
+    if (event.currentTarget.href.split("#")[0] == window.location.href.split("#")[0]) {
+      if ($("body").css("overflow") == "hidden") {
+        // This means that the content div is scrolling, not the overall page.
+        event.preventDefault();
+        $('.content').animate( { scrollTop: $('.content').scrollTop() + $(event.currentTarget.hash).position().top }, "slow");
+        // We're setting the scroll position of the content div to its current scroll position plus the relative position of the anchor link's target div.
+      } else {
+        // This means that the ovrerall page is scrolling.
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: window.pageYOffset + $(event.currentTarget.hash).position().top }, "slow");
+        // We're setting the scroll position of the page to its current scroll position plus the relative position of the anchor link's target div.
+      }
+    }
+  });
+});
